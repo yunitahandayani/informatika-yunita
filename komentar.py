61591869df9887826075283932d18693b79c27bb
yunitahandayani <nitateluk12@gmail.com>
2025-05-08 16:26:45 +0800

    tugas komentar yunita

---
+++ komentar.py
@@ -0,0 +1,12 @@
+# Ini adalah komentar satu baris
+# Program ini menjumlahkan dua angka
+
+# Mendefinisikan dua variabel
+angka1 = 5
+angka2 = 3
+
+# Menjumlahkan kedua angka
+hasil = angka1 + angka2
+
+# Menampilkan hasilnya
+print("Hasil penjumlahan:", hasil)
