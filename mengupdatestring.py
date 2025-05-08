c08c61e79ff8994229142fa8c239255f653dd4de
yunitahandayani <nitateluk12@gmail.com>
2025-05-08 16:28:42 +0800

    tugas mengupdate yunita

---
+++ mengupdatestring.py
@@ -0,0 +1,16 @@
+# Contoh sederhana mengubah string
+# Oleh: Yunita
+
+teks = "Halo, saya Yunita"
+print("Teks asli:", teks)
+
+# Ganti nama
+teks = teks.replace("Yunita", "Ayu")
+print("Teks setelah diganti:", teks)
+
+# Tambah kata di akhir
+teks = teks + " belajar Python"
+print("Teks setelah ditambah:", teks)
+
+# Ubah jadi huruf besar
+print("Huruf besar semua:", teks.upper())
